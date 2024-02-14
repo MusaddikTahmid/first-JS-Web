cbf0ad0c1f72ec49b3b2039b343e379b641a7587
MusaddikTahmid <musaddiktahmid@gmail.com>
2024-02-14 09:35:11 +0600

    rectangle

--- script/rectangle-calculate.js
+++ script/rectangle-calculate.js
@@ -1,1 +1,16 @@
-function calculateRectangleArea() {}
+function calculateRectangleArea() {
+  const rectangleLengthInput = document.getElementById("rectangle-length");
+  const lengthText = rectangleLengthInput.value;
+  const length = parseFloat(lengthText);
+  //   console.log(length);
+  const widthInput = document.getElementById("rectangle-width");
+  const widthText = widthInput.value;
+  const width = parseFloat(widthText);
+  //   console.log(width);
+  //   calculate
+  const rectangle = length * width;
+  console.log(rectangle);
+  //   rectangle
+  const rectangleAreaSapn = document.getElementById("rectangle-area");
+  rectangleAreaSapn.innerText = rectangle;
+}
